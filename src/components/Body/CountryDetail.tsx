6365effeeba33806c9bd5be16b04ec97aad7e8f9
gabrielyangzon <gab.yangzon@yahoo.com>
2022-09-23 22:32:26 +0800

    added details

--- src/components/Body/CountryDetail.tsx
+++ src/components/Body/CountryDetail.tsx
@@ -6,12 +6,18 @@ class IState {
 
 const CountryDetail = ({details} : IState) => {
      
-    
+    //console.log(Object.values(details.currencies))
+   
+    let ctryDetail : any = details !== undefined ? Object.values(details.currencies)[0] : undefined
+
+   
+   
 
     let detailsComponent = details !== undefined ? 
-                <div className='card mb-2 p-2'>
-                   <p> Continent:{details.region} </p>
-                   <p> Currency :{Object.values(details.currencies)[0].name}</p>
+                 <div className='card mb-2 p-2'>
+                    Continent:{details.region}
+                     <br/>
+                    Currency :{ctryDetail.name} ( {ctryDetail.symbol})
                 </div>
 
     : <></>
